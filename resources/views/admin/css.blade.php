da3e21d5758101b4bf956cd6e147dd7fe6391f17
Nursultan <nursultanxabipnazarov@gmail.com>
2023-12-20 22:22:46 +0500

    admin panel layaout added

---
+++ resources/views/admin/css.blade.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Otika - Admin Dashboard Template</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="admin/assets/css/app.min.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="admin/assets/css/style.css">
+  <link rel="stylesheet" href="admin/assets/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="admin/assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
